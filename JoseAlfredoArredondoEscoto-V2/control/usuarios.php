02b7e9ba0dff4732fd5e90ac78f0e08972f20d44
fredyarredondoescoto <fredyarredondoescoto@gmail.com>
2016-07-05 23:34:09 -0500

    Nueva versión de la página
    Actualizo mi página  con los archivos más recientes y las nuevas
    configuraciones que se aplicaron incluyendo la tabla PHP que muestra los
    usuarios registrados, la encriptación por MD5 y cambio del archivo
    usuarios.php a MYSQLI

--- JoseAlfredoArredondoEscoto-V2/control/usuarios.php
+++ JoseAlfredoArredondoEscoto-V2/control/usuarios.php
@@ -1,32 +1,26 @@
 <?php
-require_once 'control/datos.php';
 
-class usuarios {
-    protected $connect;
-    protected $db;
-    // Attempts to initialize the database connection using the supplied info.
-   function __construct() {
-        $this->connect = mysql_connect(DB_HOST, DB_USER, DB_PASS);
-        $this->db = mysql_select_db(DB_NAME);
-   }
+class usuarios{
+    function __construct(){
+        $this->mysqli = new mysqli("localhost", "root", "", "appweb");/*AQUI SE HACE LA CONEXION, SE ELIMINA EL ARCHIVO datos.php YA QUE LA CONEXION SE HACE DESDE AQUI*/
+    }
     
-    // Executes the specified query and returns an associative array of reseults.
-    protected function execute($sql) {
-        $res = mysql_query($sql, $this->connect) or die(mysql_error());
-		$userVO=null;
-        if(mysql_num_rows($res) > 0) {
-            for($i = 0; $i < mysql_num_rows($res); $i++) {
-                $row = mysql_fetch_assoc($res);
-                $userVO[$i] = new UserVO();
+    function execute($sql)/*LA FUNCION SE QUEDA IGUAL*/{
+    $res = $this->mysqli->query($sql);/*AQUI SE HACE LA CONSULTA (RECIBE) Y NO LLEVA PARAMETROS MAS QUE LA CONEXION $sql QUE ES LA QUE RECIBE*/
+    $userVO = null;
+    if($res->num_rows > 0)/**/{
+        for($i = 0; $i < $res->num_rows; $i++){
+            $row = $res->fetch_assoc();
+            $userVO[$i] = new UserVO();
 				//echo $row["usrid"]." ".$row["usrnombre"]." ".$row["usrpass"];
-                $userVO[$i]->setId($row["usrid"]);
-                $userVO[$i]->setUsername($row["usrnombre"]);
-                $userVO[$i]->setPassword($row["usrpass"]);
+            $userVO[$i]->setId($row["usrid"]);
+            $userVO[$i]->setUsername($row["usrnombre"]);
+            $userVO[$i]->setPassword($row["usrpass"]);
             }
         }
         return $userVO;
-	}
-    
+    }   
+
     // Retrieves the corresponding row for the specified user ID.
     public function getByUserId($userId) {
         $sql = "SELECT * FROM users WHERE id=".$userId;
@@ -35,7 +29,7 @@ class usuarios {
     
     // Retrieves all users currently in the database.
     public function getUsers() {
-        $sql = "SELECT * FROM users";
+        $sql = "SELECT * FROM tblusuarios";
         return $this->execute($sql);
     }
     
@@ -60,16 +54,16 @@ class usuarios {
                 "password='".$userVO->getPassword()."' ".
                 "WHERE id=".$userVO->getId();
             
-            mysql_query($sql, $this->connect) or die(mysql_error());
-            $affectedRows = mysql_affected_rows();
+            $this->mysqli->query($sql);
+            $affectedRows->affected_rows();
         }
         else {
             $sql = "INSERT INTO users (username, password) VALUES('".
                 $userVO->getUsername()."', ".
                 $userVO->getPassword()."')".
             
-            mysql_query($sql, $this->connect) or die(mysql_error());
-            $affectedRows = mysql_affected_rows();
+            $this->mysqli->query($sql);
+            $affectedRows->affected_rows();
         }
         return $affectedRows;
     }
@@ -87,13 +81,14 @@ class usuarios {
         if(sizeof($currUserVO) > 0) {
             $sql = "DELETE FROM users WHERE id=".$userVO->getId();
             
-            mysql_query($sql, $this->connect) or die(mysql_error());
-            $affectedRows = mysql_affected_rows();
+            $this->mysqli->query($sql);
+            $affectedRows->affected_rows();
         
         return $affectedRows;
     }
   }
 }
+
 
 class UserVO {
 	protected $id;
